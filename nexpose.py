af91f08c488bfac2753c352d5b4c68a9e33e3ff5
Steve <retail.mbox@mgail.com>
2016-12-23 19:26:44 -0700

    mostly ide workspace related

--- nexpose.py
+++ nexpose.py
@@ -1,4 +1,4 @@
-# !/bin/python
+#!/usr/bin/env python
 
 import urllib2
 import requests
@@ -17,7 +17,6 @@ def dump(obj):
 # Creates class for the client
 class Client:
     def __init__(self, server='', port=3780, api_ver='1.1', username='', password='', logger=None):
-        """ Client Class init call """
         self.server = server
         self.port = port
         self.username = username
@@ -34,8 +33,7 @@ class Client:
         # urllib2.install_opener(opener)
         # self.login()  Remove from init - now must explicitly call it (b/c it's different for each api ver)
 
-    # Request parser
-    def request(self, call, api_ver='1.1', **params):
+    def request_parser(self, call, api_ver='1.1', **params):
         """ Processes a Request for an API call """
         url = self.url_prefix + api_ver + '/xml' \
                                           ''
@@ -68,7 +66,7 @@ class Client:
 
     def login(self, api_ver='1.1'):
         """ logs you into the device """
-        response = self.request("Login")
+        response = self.request_parser("Login")
         self.authtoken = response.attrib['session-id']
 
     def get_auth_token(self):
@@ -80,23 +78,23 @@ class Client:
         return response
 
     def asset_group_config(self, groupid):
-        response = self.request("SiteConfig")
+        response = self.request_parser("SiteConfig")
         return etree.tostring(response)
 
     def asset_group_delete(self, groupid):
-        response = self.request("AssetGroupDelete")
+        response = self.request_parser("AssetGroupDelete")
         return etree.tostring(response)
 
     def asset_group_listing(self):
-        response = self.request("AssetGroupListing")
+        response = self.request_parser("AssetGroupListing")
         return etree.tostring(response)
 
     def asset_group_save(self, groupid):
-        response = self.request("AssetGroupSave")
+        response = self.request_parser("AssetGroupSave")
         return etree.tostring(response)
 
     def device_delete(self, deviceid):
-        response = self.request("DeviceDelete")
+        response = self.request_parser("DeviceDelete")
         return etree.tostring(response)
 
     def download_report(self, reporturl):
@@ -107,119 +105,119 @@ class Client:
         return resxml
 
     def engine_activity(self, engineid):
-        response = self.request("EngineActivity")
+        response = self.request_parser("EngineActivity")
         return etree.tostring(response)
 
     def engine_listing(self):
-        response = self.request("EngineListing")
+        response = self.request_parser("EngineListing")
         return etree.tostring(response)
 
     def logout(self):
-        response = self.request("Logout")
+        response = self.request_parser("Logout")
         return response.attrib['success']
 
     def report_generate(self, reportid):
-        response = self.request("ReportConfig")
+        response = self.request_parser("ReportConfig")
         return etree.tostring(response)
 
     def report_listing(self):
-        response = self.request("ReportListing")
+        response = self.request_parser("ReportListing")
         return etree.tostring(response)
 
     def report_template_listing(self):
-        response = self.request("ReportTemplateListing")
+        response = self.request_parser("ReportTemplateListing")
         return etree.tostring(response)
 
     def report_history(self, reportcfgid):
-        response = self.request("ReportHistory")
+        response = self.request_parser("ReportHistory")
         return etree.tostring(response)
 
     def restart(self):
-        response = self.request("Restart")
+        response = self.request_parser("Restart")
         return etree.tostring(response)
 
     def scan_activity(self):
-        response = self.request("ScanActivity")
+        response = self.request_parser("ScanActivity")
         return etree.tostring(response)
 
     def scan_pause(self, scanid):
-        response = self.request("ScanPause")
+        response = self.request_parser("ScanPause")
         return etree.tostring(response)
 
     def scan_resume(self, scanid):
-        response = self.request("ScanResume")
+        response = self.request_parser("ScanResume")
         return etree.tostring(response)
 
     def scan_statistics(self, scanid):
-        response = self.request("ScanStatistics")
+        response = self.request_parser("ScanStatistics")
         return etree.tostring(response)
 
     def scan_status(self, scanid):
-        response = self.request("ScanStatus")
+        response = self.request_parser("ScanStatus")
         return etree.tostring(response)
 
     def scan_stop(self, scanid):
-        response = self.request("ScanStop")
+        response = self.request_parser("ScanStop")
         return etree.tostring(response)
 
     def site_config(self, siteid):
-        response = self.request("SiteConfig")
+        response = self.request_parser("SiteConfig")
         return etree.tostring(response)
 
     def site_delete(self, siteid):
-        response = self.request("SiteDelete")
+        response = self.request_parser("SiteDelete")
         return etree.tostring(response)
 
     def site_device_listing(self, siteid):
-        response = self.request("SiteDeviceListing")
+        response = self.request_parser("SiteDeviceListing")
         return etree.tostring(response)
 
     def site_name_listing(self):
-        response = self.request("SiteListing")
+        response = self.request_parser("SiteListing")
         return response.xpath("/SiteListingResponse/SiteSummary/@name")
 
     def site_id_listing(self):
-        response = self.request("SiteListing")
+        response = self.request_parser("SiteListing")
         return response.xpath("/SiteListingResponse/SiteSummary/@id")
 
     def site_scan(self, siteid):
-        response = self.request("SiteScan")
+        response = self.request_parser("SiteScan")
         return etree.tostring(response)
 
     def site_scan_history(self, siteid):
-        response = self.request("SiteScanHistory")
+        response = self.request_parser("SiteScanHistory")
         return etree.tostring(response)
 
     def system_update(self):
-        response = self.request("SystemUpdate")
+        response = self.request_parser("SystemUpdate")
         return etree.tostring(response)
 
     def system_information(self):
-        response = self.request("SystemInformation")
+        response = self.request_parser("SystemInformation")
         return etree.tostring(response)
 
     def user_authenticator_listing(self):
-        response = self.request("UserAuthenticatorListing")
+        response = self.request_parser("UserAuthenticatorListing")
         return etree.tostring(response)
 
     def user_config(self, userid):
-        response = self.request("UserConfig")
+        response = self.request_parser("UserConfig")
         return etree.tostring(response)
 
     def user_delete(self, userid):
-        response = self.request("UserDelete")
+        response = self.request_parser("UserDelete")
         return etree.tostring(response)
 
     def user_listing(self):
-        response = self.request("UserListing")
+        response = self.request_parser("UserListing")
         return etree.tostring(response)
 
     def vulnerability_details(self, vulnid):
-        response = self.request("VulnerabilityDetails")
+        response = self.request_parser("VulnerabilityDetails")
         return etree.tostring(response)
 
     def vulnerability_listing(self):
-        response = self.request("VulnerabilityListing")
+        response = self.request_parser("VulnerabilityListing")
         return etree.tostring(response)
 
     def ad_hoc_report_request(self, call, query, site_id=[]):
